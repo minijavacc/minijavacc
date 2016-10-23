1cc4a793b56883ef9f479d2b375063843a15f6e3
Marcel Kost <mkost1@gmx.de>
2016-10-23 12:30:32 +0200

    fixed exit code of scripts, added .gitignore that ignores cmake stuff

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 # get path of this script file
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname "$(readlink -e "$0")")
+echo $BASEDIR
 
 # run cmake
-cmake ${BASEDIR}
+(cd ${BASEDIR} && cmake ${BASEDIR})
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit 1
+fi
 
 # run make
-make -C ${BASEDIR}
+(cd ${BASEDIR} && make -C ${BASEDIR})
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit 1
+fi
+
+exit 0
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # get path of this script file
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname "$(readlink -e "$0")")
 
 # run compiler with given arguments
 ${BASEDIR}/bin/compiler "$@"
