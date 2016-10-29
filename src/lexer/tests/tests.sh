9b51959e9e055ef62b6041c9ff9e1abdf1f53bd6
Markus Schlegel <mail@markus-schlegel.com>
2016-10-29 19:04:02 +0200

    Refine test script, integrate other tests, fix tests 1,2, and 5
    closes #4

--- src/lexer/tests/tests.sh
+++ src/lexer/tests/tests.sh
@@ -1,83 +1,70 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+CLEAR='\033[0m'
+
 BASEDIR=$(pwd)
 
 cd ../../..
 
 lexer=./run
 
+test_files[0]="edge_cases"
+test_files[2]="prog1"
+test_files[3]="test1"
+test_files[4]="test2"
+test_files[5]="test3"
+test_files[5]="test4"
+test_files[5]="test5"
+
+error_files[0]="errors"
 
 
 
-$lexer --lextest ${BASEDIR}/edge_cases_in.txt > ${BASEDIR}/edge_cases.out 2> ${BASEDIR}/edge_cases.err
+success=true
 
-diff ${BASEDIR}/edge_cases.out ${BASEDIR}/edge_cases_out.txt > o
 
-lo=`cat o | wc -l`
-le=`cat ${BASEDIR}/edge_cases.err | wc -l`
 
-if [ $lo -ne 0  ]; then
-  echo "Edge case tests failed (stdout)"
+for i in "${test_files[@]}"
+do
+  $lexer --lextest "${BASEDIR}/"$i"_in.txt" > "${BASEDIR}/"$i".out"
+
+  diff "${BASEDIR}/"$i".out" "${BASEDIR}/"$i"_out.txt" > o
+
+  lo=`cat o | wc -l`
+
+  if [ $lo -ne 0  ]; then
+    echo "${RED}"$i" tests failed (stdout).${CLEAR} Diff:"
+    cat o
+    echo ''
+    success=false
+  fi
+
+  rm "${BASEDIR}/"$i".out"
+  rm o
+done
+
+
+
+for i in "${error_files[@]}"
+do
+  $lexer --lextest "${BASEDIR}/"$i"_in.txt" 2> "${BASEDIR}/"$i".err"
+
+  le=`cat "${BASEDIR}/"$i".err" | grep error | wc -l`
+
+  if [ $le -lt 1  ]; then
+    echo "${RED}"$i" tests failed (stderr).${CLEAR} Should produce error but does not."
+    success=false
+  fi
+
+  rm "${BASEDIR}/"$i".err"
+done
+
+
+
+
+if [ "$success" = true ]; then
+  exit 0
+else
   exit 1
 fi
-
-if [ $le -ne 0  ]; then
-  echo "Edge case tests failed (stderr)"
-  exit 1
-fi
-
-rm ${BASEDIR}/edge_cases.out
-rm ${BASEDIR}/edge_cases.err
-
-
-
-
-
-
-$lexer --lextest ${BASEDIR}/errors_in.txt > ${BASEDIR}/errors.out 2> ${BASEDIR}/errors.err
-
-diff ${BASEDIR}/errors.out ${BASEDIR}/errors_out.txt > o
-cat ${BASEDIR}/errors.err | grep error > e
-
-lo=`cat o | wc -l`
-le=`cat e | wc -l`
-
-if [ $lo -ne 0  ]; then
-  echo "Error tests failed (stdout)"
-  exit 1
-fi
-
-if [ $le -lt 1  ]; then
-  echo "Error tests failed (stderr)"
-  exit 1
-fi
-
-rm ${BASEDIR}/errors.out
-rm ${BASEDIR}/errors.err
-
-
-
-
-
-$lexer --lextest ${BASEDIR}/Prog1.java > ${BASEDIR}/prog1.out
-
-diff ${BASEDIR}/prog1.out ${BASEDIR}/Prog1.java.lex > o
-
-lo=`cat o | wc -l`
-
-if [ $lo -ne 0  ]; then
-  echo "Prog1 tests failed (stdout)"
-  exit 1
-fi
-
-# rm ${BASEDIR}/prog1.out
-
-
-
-
-
-
-rm e
-rm o
-
-exit 0
