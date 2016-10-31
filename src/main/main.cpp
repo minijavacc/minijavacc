aaa96412975617bff304d9a00a38726cd762bd61
p3732 <p3732@users.noreply.github.com>
2016-10-31 03:34:03 +0100

    main: file access error checks

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,9 +2,31 @@
 #include "inputparser.h"
 
 #include <iostream>
+#include <functional>
 #include <fstream>
+#include <stdexcept>
 
 using namespace cmpl;
+
+
+std::ifstream readFile(const std::string& filename)
+{
+  std::ifstream file(filename);
+  if (file.good())
+  {
+    return file;
+  }
+  else
+  {
+    std::string err = "Can't read input file " + filename;
+    throw std::runtime_error(err);
+  }
+}
+
+void printException(const std::exception e)
+{
+  std::cerr << e.what();
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,27 +46,34 @@ int main(int argc, char* argv[])
   if (input.cmdOptionExists("--echo"))
   {
     const std::string &filename = input.getCmdOption("--echo");
-    if (!filename.empty())
+    std::ifstream file;
+    try
     {
-      std::ifstream file;
-      
-      file.open(filename);
+      file = readFile(filename);
       c.echo(file);
-      file.close();
     }
+    catch (std::runtime_error err)
+    {
+      printException(err);
+      return 2;
+    }
+    
   }
   
   if (input.cmdOptionExists("--lextest"))
   {
     const std::string &filename = input.getCmdOption("--lextest");
-    if (!filename.empty())
+    std::ifstream file;
+    try
     {
-      std::ifstream file;
-      
-      file.open(filename);
-      c.lextest(file);
-      file.close();
+      file = readFile(filename);
     }
+    catch (std::runtime_error err)
+    {
+      printException(err);
+      return 3;
+    }
+    c.lextest(file);
   }
   
   return 0;
