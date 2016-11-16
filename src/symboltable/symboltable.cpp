fa2cbf01e2619f30823be44474559eb21b1570df
Markus Schlegel <mail@markus-schlegel.com>
2016-11-16 17:51:14 +0100

    Add simple SymbolTable interface and implementation
    closes #17
    closes #18

--- src/symboltable/symboltable.cpp
+++ src/symboltable/symboltable.cpp
@@ -3,5 +3,66 @@
 #include <map>
 #include <string>
 #include <memory>
+#include <iostream>
+
+
+
+
 
 using namespace cmpl;
+
+
+
+// --- Scope ---
+
+std::weak_ptr<Declaration> Scope::lookup(std::string name) {
+  if (declarations->count(name) > 0) {
+    return declarations->at(name);
+  } else {
+    if (parent != nullptr) {
+      return parent->lookup(name);
+    }
+    
+    return std::weak_ptr<Declaration>();
+  }
+}
+
+Scope::Scope(std::shared_ptr<Scope> p) : parent(p), 
+                                         declarations(new std::map<std::string, std::weak_ptr<Declaration> >()) { }
+
+void Scope::insert(std::string name, std::weak_ptr<Declaration> decl) {
+  (*declarations)[name] = decl;
+}
+
+
+
+
+// --- SymbolTable ---
+
+void SymbolTable::enterScope() {
+  current = std::unique_ptr<Scope>(new Scope(current));
+}
+
+void SymbolTable::leaveScope() {
+  if (current) {
+    current = current->parent;
+  }
+}
+
+std::weak_ptr<Declaration> SymbolTable::lookup(std::string name) {
+  if (current) {
+    return current->lookup(name);
+  } else {
+    return std::weak_ptr<Declaration>();
+  }
+}
+
+bool SymbolTable::isDefinedInCurrentScope(std::string name) {
+  return current->declarations->count(name) > 0;
+}
+
+void SymbolTable::insert(std::string name, std::weak_ptr<Declaration> decl) {
+  if (current) {
+    current->insert(name, decl);
+  }
+}
--- src/symboltable/symboltable.h
+++ src/symboltable/symboltable.h
@@ -1,23 +1,38 @@
 #pragma once
 
-// forward declarations
-namespace cmpl
-{
-  class SymbolTable;
-}
-
 #include <map>
+#include <string>
 #include <memory>
 
 namespace cmpl
 {
   
+  struct Declaration {
+    std::string foo;
+  };
+  
+  class Scope {
+    public:
+      std::unique_ptr<std::map<std::string, std::weak_ptr<Declaration> > > declarations;
+      std::shared_ptr<Scope> parent;
+      Scope(std::shared_ptr<Scope> p);
+      std::weak_ptr<Declaration> lookup(std::string name);
+      void insert(std::string name, std::weak_ptr<Declaration> decl);
+  };
+  
+  
   class SymbolTable
   {
     public:
+      void enterScope();
+      void leaveScope();
+      void insert(std::string name, std::weak_ptr<Declaration> decl);
+      std::weak_ptr<Declaration> lookup(std::string name);
+      bool isDefinedInCurrentScope(std::string name);
+      SymbolTable() : current(nullptr) {}
       
     private:
-      SymbolTable() { };
+      std::shared_ptr<Scope> current;
   };
 
 }
